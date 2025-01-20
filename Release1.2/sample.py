0629ac5cc79ccb714860b0a3364e6a0b1868d77b
Ramesh-Pawar100 <105207539+Ramesh-Pawar100@users.noreply.github.com>
2025-01-20 09:03:53 +0100

    Update sample.py

--- Release1.2/sample.py
+++ Release1.2/sample.py
@@ -1,7 +1,7 @@
 """This module is used for very basic coding example to check github actions"""
 print("Hello Prague!!")
 for i in range(10):
-    print(i)    
+    print(i)
 def myfun(a,b):
     """This function is used to add two number and return sum"""
     return a+b
